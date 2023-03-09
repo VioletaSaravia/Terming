312f24e1473aafbdb5ef6b348110e3804cfdb5d4
Violeta Saravia <saraviavioleta@gmail.com>
2023-03-08 23:17:18 -0300

    MVP

--- lib-terming/src/lib.rs
+++ lib-terming/src/lib.rs
@@ -1,11 +1,12 @@
 use core::time;
+use crossterm::execute;
 use crossterm::terminal::{Clear, ClearType};
 use ndarray::{arr2, s, Array2};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{stdout, BufReader};
 use std::thread;
 
 pub trait Collidable {}
@@ -17,7 +18,7 @@ struct Coordinate {
 }
 
 #[derive(Debug)]
-struct ObjDimension {
+struct Dimension {
     x: usize,
     y: usize,
 }
@@ -28,7 +29,7 @@ type Region = Array2<char>;
 struct Object {
     placement: Coordinate,
     visual: Region,
-    size: ObjDimension,
+    size: Dimension,
     layer: usize,
 }
 
@@ -39,13 +40,13 @@ impl Object {
         // let visual: Array2<char> = serde_json::from_reader(reader).unwrap();
 
         let visual: Region = serde_json::from_str(&visual_path).unwrap();
-        let size = ObjDimension {
+        let size = Dimension {
             x: visual.shape()[0],
             y: visual.shape()[1],
         };
 
         Ok(Object {
-            placement: Coordinate { x: 0, y: 0 },
+            placement: Coordinate { x: 3, y: 3 },
             visual,
             size,
             layer: 0,
@@ -60,7 +61,7 @@ struct Scene {
 }
 
 impl Scene {
-    fn new(object: HashMap<String, Box<Object>>, resolution: ObjDimension) -> Scene {
+    fn new(object: HashMap<String, Box<Object>>, resolution: Dimension) -> Scene {
         let frame = Array2::<char>::default((resolution.x, resolution.y));
         Scene { object, frame }
     }
@@ -76,7 +77,7 @@ impl Scene {
             let x_end = if obj.placement.x < 0 {
                 obj.size.x - obj.placement.x.abs() as usize
             } else {
-                obj.size.x
+                obj.size.x + obj.placement.x as usize
             };
 
             let y_start = if obj.placement.y < 0 {
@@ -88,13 +89,15 @@ impl Scene {
             let y_end = if obj.placement.y < 0 {
                 obj.size.y - obj.placement.y.abs() as usize
             } else {
-                obj.size.y
+                obj.size.y + obj.placement.y as usize
             };
 
             let mut obj_region = self
                 .frame
                 .slice_mut(s![x_start..x_end as i32, y_start..y_end as i32]);
-            obj_region = obj.visual.view_mut();
+
+            obj_region.assign(&obj.visual.view_mut());
+            // obj_region = obj.visual.view_mut();
         }
     }
 }
@@ -104,11 +107,11 @@ struct App {
     current_scene: String,
     framerate: usize,
     frametime: u64,
-    resolution: ObjDimension,
+    resolution: Dimension,
 }
 
 impl App {
-    fn new(framerate: usize, resolution: ObjDimension, scene: HashMap<String, Box<Scene>>) -> App {
+    fn new(framerate: usize, resolution: Dimension, scene: HashMap<String, Box<Scene>>) -> App {
         App {
             scene,
             current_scene: "test_scene".to_owned(),
@@ -123,20 +126,25 @@ impl App {
         let to_display = &mut self.scene.get_mut(&self.current_scene).unwrap();
 
         to_display.render();
-        for char in &to_display.frame {
-            if char != &'\0' {
-                print!("{} ", char);
-            } else {
-                print!("_ ");
+
+        for line in to_display.frame.axis_iter(ndarray::Axis(1)) {
+            for char in line {
+                if char != &'\0' {
+                    print!("{} ", char);
+                } else {
+                    print!("_ ");
+                }
             }
+            print!("\n");
         }
     }
 
-    fn start(&mut self) -> () {
+    fn start(&mut self) -> Result<(), Box<dyn Error>> {
         loop {
             self.display();
             thread::sleep(time::Duration::from_millis(self.frametime));
-            Clear(ClearType::All);
+            let mut stdout = stdout();
+            execute!(stdout, Clear(ClearType::All))?;
         }
     }
 }
@@ -146,7 +154,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works() -> Result<(), Box<dyn Error>> {
         let cube = arr2(&[['X', 'X'], ['X', 'X']]);
         let serialized_cube = serde_json::to_string(&cube).unwrap();
 
@@ -155,12 +163,12 @@ mod tests {
         let mut obj_list = HashMap::new();
         obj_list.insert("Cube".to_owned(), Box::new(test_obj));
 
-        let test_scene = Scene::new(obj_list, ObjDimension { x: 40, y: 30 });
+        let test_scene = Scene::new(obj_list, Dimension { x: 40, y: 30 });
         let mut scene_list = HashMap::new();
         scene_list.insert("test_scene".to_owned(), Box::new(test_scene));
 
-        let mut test_game = App::new(3, ObjDimension { x: 40, y: 30 }, scene_list);
+        let mut test_game = App::new(3, Dimension { x: 40, y: 30 }, scene_list);
 
-        test_game.start();
+        test_game.start()
     }
 }
